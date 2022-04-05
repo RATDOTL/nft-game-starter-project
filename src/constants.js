24603d29d70c515f95e5c2e986bde2bfefeb3f1f
RATDOTL <ratdotl2357@gmail.com>
2022-04-05 15:14:32 +0900

    upload to github

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+// CONTRACT_ADDRESSに、コントラクトアドレスを保存します。
+const CONTRACT_ADDRESS = '0xB2B089e87c39b29202f3b0b41398bd6f82D2f2Da';
+
+//NFT キャラクター の属性をフォーマットしてオブジェクトとして返します。
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+// 変数を constants.js 以外の場所でも使えるようにします。
+export { CONTRACT_ADDRESS, transformCharacterData };
